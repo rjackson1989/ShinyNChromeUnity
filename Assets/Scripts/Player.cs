317f7aa463b893faa4e74fb06b774580bb72228e
Raymond Jackson <rjackson1989@temple.edu>
2016-04-09 01:01:06 -0400

    added enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Player : MonoBehaviour {
 
     public float speed = 10.0F;
     public float rotationSpeed = 100.0F;
     public int playerNumber = 1;
+    public Transform explosionPF;
+    public Hashtable stats;
     // Use this for initialization
     void Start () {
+        stats = new Hashtable();
+        initStats();
 	}
 
+    private void initStats()
+    {
+        
+    }
+
     // Update is called once per frame
-    
+
     void Update()
     {
         float translation = Input.GetAxis("Vertical"+playerNumber) * speed;
@@ -22,5 +32,6 @@ public class Player : MonoBehaviour {
         Vector3 forward = q * Vector3.forward;
         transform.Translate(-forward * translation);
         transform.Rotate(0, 0, rotation);
+        
     }
 }
