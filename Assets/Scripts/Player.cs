3a3e159e56e93b6a74a7612dce36de04820995a9
Raymond Jackson <rjackson1989@temple.edu>
2016-04-08 22:02:10 -0400

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    public int playerNumber = 1;
+    // Use this for initialization
+    void Start () {
+	}
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical"+playerNumber) * speed;
+        float rotation = Input.GetAxis("Horizontal" + playerNumber) * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        Quaternion q = transform.rotation;
+        Vector3 forward = q * Vector3.forward;
+        transform.Translate(-forward * translation);
+        transform.Rotate(0, 0, rotation);
+    }
+}
